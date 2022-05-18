e5b911531c790561e9b3b95f99ce7819ed1d79a5
Awais <Awaisbinmohsin@gmail.com>
2022-05-19 00:02:24 +0500

    Numbers

---
+++ Index.js
@@ -0,0 +1,32 @@
+let radius = 10;
+const pi = 3.1416;
+console.log(radius, pi);
+
+//Math operators +,-,/,**,%
+
+console.log(10/2);
+let result = radius % 3;
+let areaOfCircle = pi * radius **2;
+
+//Order of operation - B I D M A S
+
+let thisresult = 5 * (10-3)**2;
+console.log(thisresult);
+
+//Icrement and Decrement
+
+let like = 10;
+like = like +1;
+//or
+like++;
+//or
+like += 1;
+like -= 1;
+like *= 2;
+like /= 2;
+
+//NaN - not a number
+
+console.log( 5 / 'hello');
+
+console.log('This blog has '+ like +'likes');
